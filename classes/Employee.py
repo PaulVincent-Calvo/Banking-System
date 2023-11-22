ce47ef9c8735b513c6449f0e01b6cd26212dde0b
Vincent <120380476+PaulVincent-Calvo@users.noreply.github.com>
2023-11-23 04:30:20 +0800

    Update Employee.py

--- classes/Employee.py
+++ classes/Employee.py
@@ -1,57 +1,53 @@
-from DatabaseConnector import DatabaseConnector
-import mysql.connector
-import tkinter as tk
+from BankingMethods import BankingMethods
 from tkinter import messagebox
-from tkinter import ttk
+from tabulate import tabulate
+import tkinter as tk
 
-class Employee(DatabaseConnector):
+
+class Test(BankingMethods):
     def __init__(self):
-        super().__init__()  # Initialize the parent class
+        super().__init__()  
 
-    def check_database_connection(self):
-        if self.connection is not None:
-            return True
-        else:
-            return False
+    def get_transactions(self):
+        cursor = self.connection.cursor()
+        get_transactions_query = f"SELECT * from transactions"
+        cursor.execute(get_transactions_query)
 
-    def show_connection_status(self):
-        login_root = tk.Tk()
-        login_root.title("Employee Login Page")
+        transactions_rows = cursor.fetchall()
+
+        column_names = ["Transaction ID", "Checkings ID", "Date", "Time", "Amount", "Transaction Type"]
+
+        transactions = tabulate(transactions_rows, headers=column_names, tablefmt='grid')
+
+        cursor.close()
+
+        return transactions
+    
+    def get_customer_info(self):
+        cursor = self.connection.cursor()
+        get_info_query = f"SELECT * FROM customer_information JOIN checkings_account ON customer_information.customer_id = checkings_account.customer_id;"
+        cursor.execute(get_info_query)
+
+        account_info_rows = cursor.fetchall()
+
+        column_names = ["Customer ID", "Password", "First Name", "Last Name", 
+                        "Email", "Address", "ID Type", "Occupation", 
+                        "Annual Gross Income", "Checkings ID", "Balance"]
+        
+        accoount_info = tabulate(account_info_rows, headers=column_names,tablefmt="grid")
+        
+        return accoount_info
+    def test(self):
         try:
             self.connect_database()
             if self.check_database_connection():
-                self.login_page(login_root)
+                pass
 
         except Exception as e:
             messagebox.showerror("Database Connection Error", str(e))
 
-        login_root.mainloop()
-
-    def submit_login(self, login_root, employee_id_entry, employee_password_entry):
-        employee_id_entry = employee_id_entry.get()
-        employee_password_entry = employee_password_entry.get()
-
-        cursor = self.connection.cursor()
-        employee_login_query = "SELECT employee_id, employee_password FROM employees WHERE employee_id = %s"
-        cursor.execute(employee_login_query, (employee_id_entry,))
-
-        el_query_result = cursor.fetchone() 
-
-        if el_query_result:
-            stored_user_id, stored_user_password = el_query_result
-            if employee_password_entry == stored_user_password:
-                cursor.close()
-                messagebox.showinfo("Login Successful", "Login Successful")
-                login_root.destroy() 
-                self.main_page(employee_id_entry)
-            else:
-                cursor.close()
-                messagebox.showerror("Login Failed", "Incorrect ID/Password")
-        else:
-            cursor.close()
-            messagebox.showerror("Login Failed", "Incorrect ID/Password")
-
-    def login_page(self, login_root):
+    def login_page(self):
+        login_root = tk.Tk()
         login_root.geometry("300x200")
 
         login_frame = tk.Frame(login_root, width = 300, height = 200)
@@ -63,7 +59,7 @@ class Employee(DatabaseConnector):
         employee_id_entry = tk.Entry(login_frame)
         employee_password_entry = tk.Entry(login_frame, show="*")  # replaces input with "*"s for security
 
-        login_button = tk.Button(login_frame, text="Login", command=lambda: self.submit_login(login_root, employee_id_entry, employee_password_entry))
+        login_button = tk.Button(login_frame, text="Login", command=lambda: self.login(login_root, employee_id_entry, employee_password_entry, 1))
 
         # layout 
         login_frame.pack(expand=True, fill="none", side="top")
@@ -71,31 +67,43 @@ class Employee(DatabaseConnector):
         employee_id_entry.pack()
         label_password.pack()
         employee_password_entry.pack()
-
         login_button.pack(pady=20)
 
-    def main_page(self, employee_id_entry):
+        login_root.mainloop()
+
+    def main_page(self, id_entry):
         main_root = tk.Tk()
-        main_root.title("Employee Main Page")
-        main_root.geometry("500x400")
+        main_root.geometry("1400x500")
 
-        cursor = self.connection.cursor()
-        employee_name_query = "SELECT first_name, last_name FROM employees WHERE employee_id = %s"
-        cursor.execute(employee_name_query, (employee_id_entry,))
+        # frames
+        side_frame = tk.Frame(main_root, width = 300, height = 500)
+        main_frame = tk.Frame(main_root, width = 700, height = 500)
 
-        en_query_result = cursor.fetchone()
+        # content on side frame (Employee Name and Buttons)
+        first_name, last_name = self.get_name(id_entry, 1)
+        employee_name_label = tk.Label(side_frame, text=f"Welcome {first_name} {last_name}!")
+        employee_name_label.pack(side="top", pady=10)
 
-        if en_query_result:
-            first_name, last_name = en_query_result
-            employee_name_label = tk.Label(main_root, text=f"Welcome {first_name} {last_name}!")
-            employee_name_label.pack()
+        side_frame.pack(side="left", fill="y")
 
-        else: # for instances where there is an employee id and password in the database but the name isn't added
-            employee_name_label = tk.Label(main_root, text="Employee Name not found")
-            employee_name_label.pack()
+        # content on main frame (Transaction Tables)
+        transactions = self.get_customer_info()
+        transactions_text = tk.Text(main_frame, height=10, width=50)
+        transactions_text.insert(tk.END, transactions)
+        transactions_text.config(state="disabled")
+        transactions_text.pack(side="left", fill="both", padx=40, expand=True)
 
+        # scrollbar for the table if it has many content
+        scrollbar = tk.Scrollbar(main_frame, command=transactions_text.yview)
+        scrollbar.pack(side="right", fill="y")
+        transactions_text.config(yscrollcommand=scrollbar.set)
+
+        main_frame.pack(side="left", fill="both", expand=True)
+
+        #---------------------------
         main_root.mainloop()
-        
 
-employee = Employee()
-employee.show_connection_status()
+test_instance = Test()
+test_instance.test()
+# print(test_instance.get_transactions())
+test_instance.login_page()
